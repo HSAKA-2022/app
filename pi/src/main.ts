91a79d67fd42e226a31ee604e5693e41a133b90f
Tobias Chen <tobias@chen.de>
2022-08-19 18:13:53 +0200

    Added .env selecto

--- pi/src/main.ts
+++ pi/src/main.ts
@@ -3,11 +3,13 @@ import { logger } from "./log"
 import { runPollingService } from "./statePollerService"
 import { config } from "./config"
 import * as path from "path"
+import dotenv from "dotenv"
 
 /**
  * Run Pi-App
  */
 async function main() {
+    dotenv.config()
     logger.info("Starting Pi-App")
     await runPollingService()
     await runUserScripts()
@@ -19,19 +21,28 @@ async function main() {
  */
 async function runUserScripts() {
     //Read Files and filter for JS files
-    const files = readdirSync(config.userScriptPath)
-    const jsFiles = files.filter((f) => f.endsWith(".js"))
-    //Execute Files
-    logger.debug(`Files loaded are: ${jsFiles}`)
-    for (const file of jsFiles) {
-        const filePath = path.join("../", config.userScriptPath, file)
-        logger.info("Executing File: " + filePath)
-        try {
-            const module = await import(filePath)
-            await module.default()
-        } catch (error) {
-            logger.error("Error executing File: " + error)
+    if ("hostname" in process.env) {
+        const completePath = path.join(
+            config.userScriptPath,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            process.env.hostname!
+        )
+        const files = readdirSync(completePath)
+        const jsFiles = files.filter((f) => f.endsWith(".js"))
+        //Execute Files
+        logger.debug(`Files loaded are: ${jsFiles}`)
+        for (const file of jsFiles) {
+            const filePath = path.join("../", completePath, file)
+            logger.info("Executing File: " + filePath)
+            try {
+                const module = await import(filePath)
+                await module.default()
+            } catch (error) {
+                logger.error("Error executing File: " + error)
+            }
         }
+    } else {
+        logger.error("No Hostname found in .env")
     }
 }
 // eslint-disable-next-line no-console
