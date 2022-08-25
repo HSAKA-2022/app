1fb6a3477dc9920e7ef98e0d846c63cd842501c6
Tobias Chen <tobias@chen.de>
2022-08-25 09:15:20 +0200

    Fixed Linter

--- pi/update.ts
+++ pi/update.ts
@@ -3,8 +3,9 @@
 import dotenv from "dotenv"
 import { deploymentLogger } from "./src/log"
 
-var Git = require("git-wrapper")
-var git = new Git()
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Git = require("git-wrapper")
+const git = new Git()
 
 async function fetchRemotes(): Promise<void> {
     return new Promise((resolve, reject) => {
@@ -21,11 +22,11 @@ async function fetchRemotes(): Promise<void> {
 
 function getBranchName(): string {
     dotenv.config()
-    var defaultBranchName = "main"
+    const defaultBranchName = "main"
 
-    console.log(process.env.branch)
+    deploymentLogger.info("Env configured branch is: " + process.env.branch)
 
-    var branchName = process.env.branch || defaultBranchName
+    const branchName = process.env.branch || defaultBranchName
     return branchName
 }
 
@@ -38,12 +39,14 @@ function setCorrectBranch(branchName: string): void {
                 errorHandler(err, branch)
             }
             if (branch.trim() !== branchName.trim()) {
-                console.log("Not on branch " + branchName + " switching to it")
+                deploymentLogger.info(
+                    "Not on branch " + branchName + " switching to it"
+                )
                 git.exec("checkout", [branchName, "-f"], (err, msg) => {
                     if (err) {
                         errorHandler(err, msg)
                     }
-                    console.log("Checked out Branch:", msg)
+                    deploymentLogger.info("Checked out Branch:", msg)
                 })
             }
         }
@@ -79,7 +82,7 @@ async function checkForUpdate() {
     setCorrectBranch(branchName)
 
     const upstreamPath: string = "origin/" + branchName
-    const localPath: string = "@"
+    const localPath = "@"
 
     const localHash: string = await getCurrentCommitHash(localPath)
     const remoteHash: string = await getCurrentCommitHash(upstreamPath)
