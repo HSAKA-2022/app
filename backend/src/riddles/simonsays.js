0967c97055968693c21ed9b76b4cf89397054e9e
DESKTOP-05QJ07K\TN <jan.krumrein+2@opencreek.tech>
2022-08-26 10:32:02 +0200

    added simonsays backend ecma basic funcs

---
+++ backend/src/riddles/simonsays.js
@@ -0,0 +1,38 @@
+import { riddle } from "../riddle"
+
+const riddleId = "guess"
+
+function randomInt(max=4) { 
+    return Math.floor(Math.random() * max);
+}
+
+export default riddle({
+    riddleId,
+    start: (existing) => {
+        if (existing.length === 0) {
+            return {
+                sequence: [randomInt(), randomInt(), randomInt()],
+                inGame: true
+            }            
+        }
+    },
+    solved: (states) => {
+        return !states[0].state.inGame
+    },
+    getter: (state) => {
+
+        return {
+            sequenceLength: state.active.sequence.length
+        }
+    },
+    phoneActions: {
+        submit: async (players, input) => {
+            players.active.state.inGame = input.playerSequence == players.active.state.sequence
+            players.active.state.sequence.push(randomInt())
+            return players
+        },
+    },
+    piActions: {
+        
+    },
+})
