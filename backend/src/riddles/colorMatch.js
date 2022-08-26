8ae9cf942688f3ab87cfde365b8601e965274cb5
Philipp <philipp.semmel@t-online.de>
2022-08-26 09:24:01 +0200

    ColorMatch riddle file added

---
+++ backend/src/riddles/colorMatch.js
@@ -0,0 +1,18 @@
+import { riddle } from "../riddle"
+
+const riddleId = "colorMatch"
+
+export default riddle({
+    riddleId,
+    start: (players) => {
+
+    },
+    solved: (players) => {
+    },
+    getter: (players) => {
+    },
+    phoneActions: {
+    },
+    piActions: {
+    },
+})
