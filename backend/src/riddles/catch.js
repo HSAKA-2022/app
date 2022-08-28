e3437bd5d87b42cc68c80abede0688ad0c13319e
ArwenUndomiel <arwensophia.fischle@gmail.com>
2022-08-28 10:56:06 +0200

    catch

--- backend/src/riddles/catch.js
+++ backend/src/riddles/catch.js
@@ -2,17 +2,32 @@ import { riddle } from "../riddle"
 
 const riddleId = "catch"
 const informatiker = {
+    "ceb3b59a-2523-11ed-861d-0242ac120002":{
+        name: Ada,
+    },
+    "ceb3b6bc-2523-11ed-861d-0242ac120002":{
+        name: Alex,
+    },
     "ceb3a3d4-2523-11ed-861d-0242ac120002": {
-        name: Sophia,
+        name: Bene,
     },
     "ceb3a672-2523-11ed-861d-0242ac120002": {
         name: Björn,
     },
+    "ceb3b7e8-2523-11ed-861d-0242ac120002":{
+        name: Emil,
+    },
     "ceb3a906-2523-11ed-861d-0242ac120002": {
         name: Farida,
     },
-    "ceb3aa1e-2523-11ed-861d-0242ac120002":{
-        name:Toby,
+    "ceb3bf04-2523-11ed-861d-0242ac120002":{
+        name: Hannes,
+    },
+    "ceb3ad8e-2523-11ed-861d-0242ac120002":{
+        name: Hilmar,
+    },
+    "ceb3bdc4-2523-11ed-861d-0242ac120002":{
+        name: Jan,
     },
     "ceb3ab40-2523-11ed-861d-0242ac120002":{
         name:Katharina,
@@ -20,8 +35,8 @@ const informatiker = {
     "ceb3ac6c-2523-11ed-861d-0242ac120002":{
         name: Leon,
     },
-    "ceb3ad8e-2523-11ed-861d-0242ac120002":{
-        name: Hilmar,
+    "30ceb3b356-2523-11ed-861d-0242ac120002UV":{
+        name: Lukas,
     },
     "ceb3affa-2523-11ed-861d-0242ac120002":{
         name: Mischa,
@@ -29,38 +44,22 @@ const informatiker = {
     "ceb3b126-2523-11ed-861d-0242ac120002":{
         name: Philipp,
     },
+    "ceb3b932-2523-11ed-861d-0242ac120002":{
+        name: Sophia,
+    },
+    "ceb3aa1e-2523-11ed-861d-0242ac120002":{
+        name:Toby,
+    },
     "ceb3b248-2523-11ed-861d-0242ac120002":{
         name: Toplink,
     },
-    "30ceb3b356-2523-11ed-861d-0242ac120002UV":{
-        name: Lukas,
-    },
-    "ceb3b59a-2523-11ed-861d-0242ac120002":{
-        name: Ada,
-    },
-    "ceb3b6bc-2523-11ed-861d-0242ac120002":{
-        name: Alex,
-    },
-    "ceb3b7e8-2523-11ed-861d-0242ac120002":{
-        name: Emil,
-    },
-    "ceb3b932-2523-11ed-861d-0242ac120002":{
-        name: Bene,
-    },
-    "ceb3bdc4-2523-11ed-861d-0242ac120002":{
-        name: Jan,
-    },
-    "ceb3bf04-2523-11ed-861d-0242ac120002":{
-        name: Hannes,
-    },
-
 }
 
 export default riddle({
     riddleId,
     start: (existingPlayers) => {
         return {
-            infoIDs: [],
+             infoIDs: {}
         }
     },
     solved: (players) => {
@@ -83,7 +82,7 @@ export default riddle({
             for (const ID in informatiker) {
                 if (input.infoID === ID) {
                     if (!players.active.state.infoIDs.includes(input.infoID))
-                        players.active.state.infoIDs.push(input.infoID)
+                        players.active.state.infoIDs[input.infoID]=true
                 }
             }
             return players
--- frontend/pages/catch.vue
+++ frontend/pages/catch.vue
@@ -1,15 +1,100 @@
 <template>
-    <layout v-if="!state.solved">
-        Rate eine Zahl zwischen 0 und 100:
-        <input class="input mt-2" type="number" placeholder="Zahl eingeben" v-model="numberInput" />
+    <layout >
+        <h1>Gotta Catch'em All</h1>
+        <h6>01001001 01101110 01100110 01101111 00100000 01000101 01100100 01101001 01110100 01101001 01101111 01101110 </h6>
+        <table class="table table is-bordered table is-fullwidth">
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <th v-if="!state.infoIDs['ceb3b59a-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3b59a-2523-11ed-861d-0242ac120002']"> Ada </th>
+                </tr>
+                <tr>
+                    <th>2</th>
+                    <th v-if="!state.infoIDs['ceb3b6bc-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3b6bc-2523-11ed-861d-0242ac120002']"> Alex </th>
+                </tr>
+                <tr>
+                    <th>3</th>
+                    <th v-if="!state.infoIDs['ceb3a3d4-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3a3d4-2523-11ed-861d-0242ac120002']"> Bene </th>
+                </tr>
+                <tr>
+                    <th>4</th>
+                    <th v-if="!state.infoIDs['ceb3a672-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3a672-2523-11ed-861d-0242ac120002']"> Björn </th>
+                </tr>
+                <tr>
+                    <th>5</th>
+                    <th v-if="!state.infoIDs['ceb3b7e8-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3b7e8-2523-11ed-861d-0242ac120002']"> Emil </th>
+                </tr>
+                <tr>
+                    <th>6</th>
+                    <th v-if="!state.infoIDs['ceb3a906-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3a906-2523-11ed-861d-0242ac120002']"> Farida </th>
+                </tr>
+                <tr>
+                    <th>7</th>
+                    <th v-if="!state.infoIDs['ceb3bf04-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3bf04-2523-11ed-861d-0242ac120002']"> Hannes </th>
+                </tr>
+                <tr>
+                    <th>8</th>
+                    <th v-if="!state.infoIDs['ceb3ad8e-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3ad8e-2523-11ed-861d-0242ac120002']"> Hilmar </th>
+                </tr>
+                <tr>
+                    <th>9</th>
+                    <th v-if="!state.infoIDs['ceb3bdc4-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3bdc4-2523-11ed-861d-0242ac120002']"> Jan </th>
+                </tr>
+                <tr>
+                    <th>10</th>
+                    <th v-if="!state.infoIDs['ceb3ab40-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3ab40-2523-11ed-861d-0242ac120002']"> Katharina </th>
+                </tr>
+                <tr>
+                    <th>11</th>
+                    <th v-if="!state.infoIDs['ceb3ac6c-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3ac6c-2523-11ed-861d-0242ac120002']"> Leon </th>
+                </tr>
+                <tr>
+                    <th>12</th>
+                    <th v-if="!state.infoIDs['30ceb3b356-2523-11ed-861d-0242ac120002UV']"> ??? </th>
+                    <th v-if="state.infoIDs['30ceb3b356-2523-11ed-861d-0242ac120002UV']"> Lukas </th>
+                </tr>
+                <tr>
+                    <th>13</th>
+                    <th v-if="!state.infoIDs['ceb3affa-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3affa-2523-11ed-861d-0242ac120002']"> Mischa </th>
+                </tr>
+                <tr>
+                    <th>14</th>
+                    <th v-if="!state.infoIDs['ceb3b126-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3b126-2523-11ed-861d-0242ac120002']"> Philipp </th>
+                </tr>
+                <tr>
+                    <th>15</th>
+                    <th v-if="!state.infoIDs['ceb3b932-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3b932-2523-11ed-861d-0242ac120002']"> Sophia </th>
+                </tr>
+                <tr>
+                    <th>16</th>
+                    <th v-if="!state.infoIDs['ceb3aa1e-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3aa1e-2523-11ed-861d-0242ac120002']"> Toby </th>
+                </tr>
+                <tr>
+                    <th>17</th>
+                    <th v-if="!state.infoIDs['ceb3b248-2523-11ed-861d-0242ac120002']"> ??? </th>
+                    <th v-if="state.infoIDs['ceb3b248-2523-11ed-861d-0242ac120002']"> Toplink </th>
+                </tr>
 
-        <button class="button is-primary mt-2" @click="guessNumber">Zahl raten</button>
-
-        <h1 v-if="state.guess != undefined">Du hast zuletzt {{ state.guess }} geraten.</h1>
+            </tbody>
+        </table>
     </layout>
 
-    <layout v-if="state.solved">
-        <h1 class="is-size-1 has-text-success">Du hast richtig geraten!</h1>
+    <layout>
     </layout>
 </template>
 
@@ -28,7 +113,9 @@ export default {
 
     data() {
         return {
-            state: {},
+            state: {
+                infoIDs: {}
+            },
             numberInput: undefined,
         }
     },
