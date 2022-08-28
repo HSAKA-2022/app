5913e55aae91b9197f3399ce308b102fcfeffd61
ArwenUndomiel <arwensophia.fischle@gmail.com>
2022-08-28 09:13:38 +0200

    Create catch.vue

---
+++ frontend/pages/catch.vue
@@ -0,0 +1,48 @@
+<template>
+    <layout v-if="!state.solved">
+        Rate eine Zahl zwischen 0 und 100:
+        <input class="input mt-2" type="number" placeholder="Zahl eingeben" v-model="numberInput" />
+
+        <button class="button is-primary mt-2" @click="guessNumber">Zahl raten</button>
+
+        <h1 v-if="state.guess != undefined">Du hast zuletzt {{ state.guess }} geraten.</h1>
+    </layout>
+
+    <layout v-if="state.solved">
+        <h1 class="is-size-1 has-text-success">Du hast richtig geraten!</h1>
+    </layout>
+</template>
+
+<style>
+</style>
+
+<script>
+import { startRiddle, doRiddleAction, getRiddleState } from "../utils"
+
+const riddleId = "guess"
+
+export default {
+    async mounted() {
+        this.state = await startRiddle(riddleId)
+    },
+
+    data() {
+        return {
+            state: {},
+            numberInput: undefined,
+        }
+    },
+    methods: {
+        async guessNumber() {
+            if (this.numberInput == undefined) {
+                return
+            }
+
+            this.state = await doRiddleAction(riddleId, "makeAGuess", {
+                guess: this.numberInput,
+            })
+        }
+    }
+
+}
+</script>
