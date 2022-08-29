9c769f8a4d068c88fa22e2880935406bc1c0acab
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2022-08-29 09:22:45 +0200

    Fix tick function with empty state

--- backend/src/riddle.ts
+++ backend/src/riddle.ts
@@ -308,6 +308,9 @@ export function riddle<DB_STATE, API_STATE, LEADERBOARD_STATE = unknown>({
     if (tick) {
         setInterval(async () => {
             const state = await getRiddleState<DB_STATE>(riddleId)
+            if (state.length === 0) {
+                return
+            }
             const newState = await tick(state)
             if (newState != undefined) {
                 const promises = newState.map(
