1e61c95e860b63f321c2da84f2d13f6b678c3d2b
Toplink <toplinkli@outlook.com>
2022-08-31 11:43:06 +0200

    change light color

--- pi/scripts/Raspi-2/simonpi.js
+++ pi/scripts/Raspi-2/simonpi.js
@@ -2,6 +2,7 @@ import { registerCallback } from "../../src/statePollerService"
 import { triggerAction } from "../../src/actionDispatch"
 import { logger } from "../../src/log"
 import { v3 } from "node-hue-api"
+import sampleUserScript from "../sampleUserScript"
 
 const apiPromise = v3.api
     .createLocal("192.168.5.116")
@@ -30,15 +31,27 @@ async function callActionOnRiddle1() {
  * @param {Object} newState JSON object representing the new State
  */
 async function changeColors(newState) {
-    const green = new LightState().rgb(103, 190, 97).alertShort()
-    const api = await apiPromise
-
-    for (let i = 0; i < newState.all[0].state.sequence.length; i++) {}
     const lampOne = 12
     const lampTwo = 5
     const lampThree = 8
     const lampFour = 9
-    await api.lights.setLightState(lampOne, new LightState().off())
-    await sleep(100)
-    await api.lights.setLightState(lampOne, green)
+    const green = new LightState().rgb(103, 190, 97).alertShort()
+    const api = await apiPromise
+
+    for (let i = 0; i < newState.all[0].state.sequence.length; i++) {
+        if (newState.all[0].state.sequence[i] === 0) {
+            await api.lights.setLightState(lampOne, red)
+            console.log("Setting Lamp 1 to red")
+        } else if (newState.all[0].state.sequence[i] === 1) {
+            await api.lights.setLightState(lampTwo, blue)
+            console.log("Setting Lamp 2 to blue")
+        } else if (newState.all[0].state.sequence[i] === 2) {
+            await api.lights.setLightState(lampThree, yellow)
+            console.log("Setting Lamp 3 to yellow")
+        } else if (newState.all[0].state.sequence[i] === 3) {
+            await api.lights.setLightState(lampFour, green)
+            console.log("Setting Lamp 4 to green")
+        }
+        await sleep(500)
+    }
 }
