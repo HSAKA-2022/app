aafad0ce5cca84027fbcbf060a5a5a57290ecd5b
adake21 <112064995+adake21@users.noreply.github.com>
2022-08-31 16:53:38 +0300

    Reaktionstest (#16)
    * Add Backend Files
    
    * Add frontend files
    
    * Update Frontend Files
    
    * Fix undefined reactionspeed
    
    * Add final changes
    
    * add pre-leaderboard working version
    
    * Add Name Input and fix Leaderboard
    
    * resolve conflict with main
    
    Co-authored-by: Luxon <kohl.alu@gmail.com>

---
+++ frontend/pages/reaktionstest.vue
@@ -0,0 +1,131 @@
+<template>
+    <layout v-if="!state.solved && !earlyClickNotification" class="buttons are-large">
+        <h1 class="is-size-1 has-text-success">Reaktionstest</h1>
+
+        <p>Willkommen, der Reaktionstest funktioniert, indem man auf den Button klickt, sobald dieser Grün wird. Das Ziel ist innerhalb von 300ms zu reagieren. Viel Spaß ;)</p>
+        <p v-if="!this.isPlayerNameSubmitted" class="has-text-weight-bold">Gib deinen Namen ein: </p>
+
+        <button v-if="!showPictureBool && buttonhide" class="chadButton button is-primary mt-2 mx-1 is-size-1" @click="earlyclick" style="background-color: #f44336;">{{ this.recentreactionspeed === 0 ? "..." : this.recentreactionspeed }} ms </button>
+
+        <button v-if="showPictureBool" class="chadButton button is-primary mt-2 mx-1 is-size-1" @click="react" style="background-color: #4CAF50;">{{ this.recentreactionspeed === 0 ? "..." : this.recentreactionspeed }} ms </button>
+
+        <input v-if="!this.isPlayerNameSubmitted" type="text" class="input is-primary mr-2" v-model="this.playername">
+        
+        <button v-if="!this.isPlayerNameSubmitted" :disabled="this.playername === undefined || this.playername.length < 1" class="button is-primary mt-2 mx-1" @click="sendPlayerName">Namen absenden</button>
+
+        <button v-if="!buttonhide" :disabled="!this.isPlayerNameSubmitted" class="chadButton button is-primary mt-2 mx-1 is-size-1" @click="wait">START</button>
+
+        <h1 v-if="state.reactionspeed != undefined">Du hast zuletzt {{ this.recentreactionspeed === 0 ? "..." : this.recentreactionspeed }} ms gebraucht.</h1>
+        <h1 v-if="state.reactionspeed === undefined">Du hast den Reaktionstest noch nicht absolviert.</h1>
+
+    </layout>
+
+    <layout v-if="earlyClickNotification" class="buttons are-large">
+
+        <h1 v-if="earlyClickNotification" class="is-size-1 has-text-danger-dark">Du hast zu früh geklickt! Klicke erst, wenn der Button grün wird!</h1>
+        <button class="chadButton button is-primary mt-2 is-size-1" @click="retry">Nochmal versuchen</button>
+
+    </layout>
+
+    <layout v-if="state.solved" class="buttons are-large">
+        <h1 class="is-size-1 has-text-success">Du hast {{ state.reactionspeed }}ms gebraucht!</h1>
+        <button class="chadButton button is-primary mt-2 is-size-1" @click="showLeaderboard">Leaderboard</button>
+    </layout>
+</template>
+
+<style>
+    .chadButton {
+        width: 90%;
+        height: 500px;
+    }
+
+</style>
+
+<script>
+import consolaGlobalInstance from "consola"
+import { startRiddle, doRiddleAction, getRiddleState, getLeaderboard } from "../utils"
+
+const riddleId = "reaktionstest"
+
+export default {
+    async mounted() {
+        console.log("start")
+        this.state = await startRiddle(riddleId)
+        this.state.reactionspeed = this.state.reactionspeed || "..."
+
+    },
+
+    data() {
+        return {
+            state: {},
+            reactionspeed: 0,
+            firstDate: undefined,
+            secondDate: undefined,
+            showPictureBool: false,
+            buttonhide: false,
+            activeTimer: false,
+            earlyClickNotification: false,
+            waitTime: 0,
+            leaderboard: undefined,
+            recentreactionspeed: 0,
+            playername: undefined,
+            isPlayerNameSubmitted: false,
+        }
+    },
+    methods: {
+        async sendPlayerName() {
+            this.state = await doRiddleAction(riddleId, "getName", {
+            playername: this.playername,
+            })
+            this.isPlayerNameSubmitted = true
+        },
+        showPicture() {
+            this.firstDate = new Date(),
+            this.showPictureBool = true
+            
+        },
+        wait() {
+            this.waitTime =  Math.random() * 3500 + 1000;
+            this.buttonhide = true
+            this.activeTimer = setTimeout(() => {
+                this.showPicture()
+            }, this.waitTime);
+        },
+        earlyclick() {
+            clearTimeout(this.activeTimer)
+            this.earlyClickNotification = true
+            this.activeTimer = setTimeout(() => {
+                this.showPictureBool = false
+                this.buttonhide = false
+            }, 400);
+
+        },
+        async react() {
+            this.wait()
+            this.secondDate = new Date()
+            this.showPictureBool = false
+
+            this.recentreactionspeed = this.secondDate - this.firstDate 
+            this.reactionspeed = this.reactionspeed === 0 ? 9999999999999 : this.reactionspeed
+            if (this.recentreactionspeed <= this.reactionspeed) {
+                this.reactionspeed = this.recentreactionspeed
+                this.state = await doRiddleAction(riddleId, "react", {
+                reactionspeed: this.reactionspeed,
+            })
+            }
+
+        },
+        retry() {
+            this.earlyClickNotification = false
+
+
+
+        },
+        async showLeaderboard() {
+            this.leaderboard = await getLeaderboard(riddleId)
+            console.log(this.leaderboard)
+        }
+    }
+
+}
+</script>
