6b1f5a80467c04e7ba3ca09ac9daa41b83d8e0f8
Toplink <toplinkli@outlook.com>
2022-08-31 10:56:33 +0200

    make a test

--- pi/scripts/Raspi-2/simonpi.js
+++ pi/scripts/Raspi-2/simonpi.js
@@ -3,9 +3,9 @@ import { triggerAction } from "../../src/actionDispatch"
 import { logger } from "../../src/log"
 import { v3 } from "node-hue-api"
 
-const HUE_HOST = "192.168.5.116"
-const USERNAME = "AT0cUBe8aB9zedDIkfkVi3l9jOkRwIrVhfzqTZlm"
-const api = v3.api.createLocal(HUE_HOST).connect(USERNAME)
+const api = await v3.api
+    .createLocal("192.168.5.116")
+    .connect("AT0cUBe8aB9zedDIkfkVi3l9jOkRwIrVhfzqTZlm")
 /**
  * Entrypoint into the Script
  */
@@ -31,14 +31,22 @@ async function callActionOnRiddle1() {
  * @param {Object} newState JSON object representing the new State
  */
 async function changeColors(newState) {
-    const state = new LightState().on().ct(200)
-    const LIGHT_ID = 12
-
-    await api.lights.setLightState(LIGHT_ID, state)
+    const green = new LightState().rgb(103, 190, 97).alertShort()
 
     for (let i = 0; i < newState.all[0].state.sequence.length; i++) {}
-    logger.verbose("Handling State change for Riddle 1")
-    logger.debug("Received new State1: " + JSON.stringify(newState))
+    const lampOne = 12
+    const lampTwo = 5
+    const lampThree = 8
+    const lampFour = 9
+    for (let i = 0; i < 20; i++) {
+        try {
+            console.log("Setting light " + i)
+            await api.lights.setLightState(lampOne, new LightState().off())
+            await sleep(100)
+            await api.lights.setLightState(lampOne, green)
+        } catch (e) {}
+        await sleep(1000)
+    }
 }
 /**
  * Dummy Callback function
