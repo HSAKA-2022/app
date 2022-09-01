301c3722f5dc61d323ca80d212c9f5750a48207d
Tobias Chen <tobias@chen.de>
2022-09-01 13:05:03 +0200

    Fix Imports/

--- backend/src/riddles/colorMatch.js
+++ backend/src/riddles/colorMatch.js
@@ -1,5 +1,5 @@
 import { riddle } from "../riddle"
-import Color from "colorjs.io"
+import * as Color from "colorjs.io"
 
 const riddleId = "colormatch"
 
@@ -33,13 +33,13 @@ export default riddle({
                 return false
             }
         }
-        const goal = new Color.default(
+        const goal = new Color(
             "sRGB",
             players[0].state.goal,
             players[1].state.goal,
             players[2].state.goal
         )
-        const current = new Color.default("sRGB", [
+        const current = new Color.Color("sRGB", [
             players[0].state.current,
             players[1].state.current,
             players[2].state.current,
